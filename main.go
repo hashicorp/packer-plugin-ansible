d6d613c87b29935f9ef4d1a16950c790ea6e415b
Adrien Delorme <azr@users.noreply.github.com>
2021-09-01 10:35:07 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -4,32 +4,18 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/hashicorp/packer-plugin-sdk/plugin"
+
 	ansible "github.com/hashicorp/packer-plugin-ansible/provisioner/ansible"
 	ansibleLocal "github.com/hashicorp/packer-plugin-ansible/provisioner/ansible-local"
-
-	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.0"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+	"github.com/hashicorp/packer-plugin-ansible/version"
 )
 
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterProvisioner(plugin.DEFAULT_NAME, new(ansible.Provisioner))
 	pps.RegisterProvisioner("local", new(ansibleLocal.Provisioner))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 
 	if err != nil {
