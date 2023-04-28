b07adcf790cd121f06f77792e7a7304aa224af3f
Rodolphe CHAIGNEAU <rodolphe.chaigneau@gmail.com>
2023-04-28 20:26:16 +0200

    Fix missing space between ssh extra args (#150)
    * Fix missing space between ssh extra args
    
    * Add missing quotes around ssh extra args
    
    * Use fmt.Sprintf

--- provisioner/ansible/provisioner.go
+++ provisioner/ansible/provisioner.go
@@ -774,11 +774,7 @@ func (p *Provisioner) createCmdArgs(httpAddr, inventory, playbook, privKeyFile s
 	if p.generatedData["ConnType"] == "ssh" && len(privKeyFile) > 0 {
 		// Add ssh extra args to set IdentitiesOnly
 		if len(p.config.AnsibleSSHExtraArgs) > 0 {
-			var sshExtraArgs string
-			for _, sshExtraArg := range p.config.AnsibleSSHExtraArgs {
-				sshExtraArgs = sshExtraArgs + sshExtraArg
-			}
-			args = append(args, "--ssh-extra-args", fmt.Sprintf("'%s'", sshExtraArgs))
+			args = append(args, "--ssh-extra-args", fmt.Sprintf("'%s'", strings.Join(p.config.AnsibleSSHExtraArgs, " ")))
 		} else {
 			args = append(args, "--ssh-extra-args", "'-o IdentitiesOnly=yes'")
 		}
