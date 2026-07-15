cd2f8f76ead1c0b77458fec3b2419efa4cb27d7e
eshan-tripathi <eshan.tripathi@ibm.com>
2026-07-15 13:02:41 +0530

    prepare v.1.1.7-dev

--- version/version.go
+++ version/version.go
@@ -6,8 +6,8 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "1.1.6"
-	VersionPrerelease = ""
+	Version           = "1.1.7"
+	VersionPrerelease = "dev"
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
 )
